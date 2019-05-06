43544238b6e436157a0ac2d47d4dd02d07a70449
Stefan Stekelenburg <stefan.stekelenburg@gmail.com>
2019-05-06 08:34:55 +0200

    Thema
    WordPress Thema

---
+++ newtheme/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php wp_head();?>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body <?php body_class();?>>
+    <header class="sticky-top">
+      <div class="container">
+        <?php wp_nav_menu (
+          array(
+            'theme_location' => 'top-menu',
+            'menu_class' => 'navigation'
+          )
+        );?>
+      </div>
+    </header>
