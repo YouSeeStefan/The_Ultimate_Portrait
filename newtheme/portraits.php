223cb979697289f7a50e75b9039dc28a9c5e4435
Frank van der Velde <frankvdv1998@gmail.com>
2019-06-06 00:59:27 +0200

    Portraits archive responsive start

--- newtheme/portraits.php
+++ newtheme/portraits.php
@@ -9,47 +9,53 @@
 <div class="container-fluid">
   <div class="row flex-nowrap">
     <div class="col-md col-12">
-    <h1 class="portraitsTitle"><?php the_title(); ?></h1>
+   
     <div class="row">
       <div class="col">
+      <h1 class="portraitsTitle"><?php the_title(); ?></h1>
       <?php
       $args = array('post_type' => 'portrait', 'posts_per_page' => -1);
       $loop = new WP_Query($args);
       while ($loop->have_posts()) : $loop->the_post();
        
         $singlePageLink = get_permalink();
-        echo '<div class="portrait_results">';
+        echo '<div class="row portrait_results">';
         ?>
 
         <?php
 
         $image = get_field('portrait');
 
-        $size = 'medium';
+        $size = 'large';
         $thumb = $image['sizes'][$size];
 
         if (!empty($image)) : ?>
 
-          <div class="">
+          <div class="results_image">
           <a href="<?php echo $singlePageLink; ?>" class="pageLinkImage">
-          <img src="<?php echo $thumb; ?>" alt="<?php echo $image['alt']; ?>" class="imgContain" />
+          <img src="<?php echo $thumb; ?>" alt="<?php echo $image['alt']; ?>" class="resultsImage" />
           </a>
           </div>
 
         <?php endif;
         $text = get_field('description');
-        $text = substr($text, 0, 590); //587
-        $text = substr($text, 0, strrpos($text, ' '));
+        $textBig = substr($text, 0, 330); //587
+        $textBig = substr($textBig, 0, strrpos($textBig, ' '));
+
+        $textMedium = substr($text, 0, 184); //587
+        $textMedium = substr($textMedium, 0, strrpos($textMedium, ' '));
         ?>
-          <div class="resultsTextDiv">
+          <div class="results_text">
+
             <h2><?php the_field('title'); ?></h2>
             <span class="portraitInfo"><?php the_field('dimension'); ?></span>
             <span class="portraitInfo"><?php the_field('year'); ?></span>
-            <p><?php echo $text . "..." ?></p>
-        <a href="<?php echo $singlePageLink; ?>"><span class="goToPortrait">Go to the portrait</span><span class="goToArrow"><?php
+            <p class="textBig"><?php echo $textBig . "..." ?></p>
+            <p class="textMedium"><?php echo $textMedium . "..." ?></p>
+        <a href="<?php echo $singlePageLink; ?>"><span class="goToPortrait">Go to the portrait</span><span class="viewPortrait">View portrait</span><span class="goToArrow"><?php
         
         $linkStart = get_template_directory_uri();
-       echo '<img class="arrowright" src="'. $linkStart .'/img/arrow-right-solid.svg" style="height: 1em;" alt="arrowRight">';
+       echo '<img class="arrowright" src="'. $linkStart .'/img/arrow-right-solid.svg" alt="arrowRight">';
         
         ?></span></a>
         </div>
--- newtheme/single-portrait.php
+++ newtheme/single-portrait.php
@@ -7,82 +7,82 @@
   <div class="row flex-nowrap">
     <div class="col-md col-12">
       <div class="row" style="margin-top: 3em;">
-      <div class="col-md-6">
-      <div class="col-12">
-          <?php
-          $args = array('post_type' => 'portrait', 'posts_per_page' => -1);
-          $loop = new WP_Query($args);
-
-          $image = get_field('portrait');
-
-          $size = 'largest';
-          $thumb = $image['sizes'][$size];
-
-          if (!empty($image)) : ?>
-
-            <div class="">
-
-              <img src="<?php echo $thumb; ?>" alt="<?php echo $image['alt']; ?>" class="" style="width: 100%;" />
-
-            </div>
-
-          <?php endif; ?>
-        </div>
-        
-        <div class="col-12 galleryDesktop">
-          <h2 style="display: block; font-size: auto;">Progress</h2>
-          <?php
-
-          $images = get_field('process_images_gallery');
-
-          if ($images) : ?>
-
-            <?php foreach ($images as $image) : ?>
-
-              <!-- <a href="<?php echo $image['url']; ?>"> -->
-              <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
-              <!-- </a> -->
-
-
+        <div class="col-md-6">
+          <div class="col-12">
             <?php
-          endforeach;
+            $args = array('post_type' => 'portrait', 'posts_per_page' => -1);
+            $loop = new WP_Query($args);
 
-        endif;
-        previous_post_link();
-        next_post_link();
-        ?>
-        </div>
-      </div>
-      <div class="col-md-6">
-      <div class="col-12  text-center">
-          <div class="home_text">
-            <div class="resultsTextDiv">
-              <h2><?php the_field('title'); ?></h2>
-              <span class="portraitInfo"><?php the_field('dimension'); ?></span>
-              <span class="portraitInfo"><?php the_field('year'); ?></span>
-              <p><?php the_field('description'); ?></p>
+            $image = get_field('portrait');
 
-            </div>
+            $size = 'largest';
+            $thumb = $image['sizes'][$size];
+
+            if (!empty($image)) : ?>
+
+              <div class="">
+
+                <img src="<?php echo $thumb; ?>" alt="<?php echo $image['alt']; ?>" class="" style="width: 100%;" />
+
+              </div>
+
+            <?php endif; ?>
+          </div>
+
+          <div class="col-12 galleryDesktop">
+            <h2 style="display: block; font-size: auto;">Progress</h2>
+            <?php
+
+            $images = get_field('process_images_gallery');
+
+            if ($images) : ?>
+
+              <?php foreach ($images as $image) : ?>
+
+                <!-- <a href="<?php echo $image['url']; ?>"> -->
+                <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                <!-- </a> -->
+
+
+              <?php
+            endforeach;
+
+          endif;
+          previous_post_link();
+          next_post_link();
+          ?>
           </div>
         </div>
-        <div class="col-12">
-          <?php
+        <div class="col-md-6">
+          <div class="col-12  text-center">
+            <div class="home_text">
+              <div class="resultsTextDiv">
+                <h2><?php the_field('title'); ?></h2>
+                <span class="portraitInfo"><?php the_field('dimension'); ?></span>
+                <span class="portraitInfo"><?php the_field('year'); ?></span>
+                <p><?php the_field('description'); ?></p>
 
-          $image = get_field('stopmotion');
+              </div>
+            </div>
+          </div>
+          <div class="col-12">
+            <?php
 
-          if (!empty($image)) : ?>
+            $image = get_field('stopmotion');
 
-            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" style="width: 100%; border: 1px solid black;" />
+            if (!empty($image)) : ?>
 
-          <?php endif; ?>
+              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" style="width: 100%; border: 1px solid black;" />
 
+            <?php endif; ?>
+
+          </div>
         </div>
-      </div>
-        
 
- 
-        
-      <div class="col-12 galleryMobile">
+
+
+
+        <div class="col-12 galleryMobile">
           <h2 style="display: block; font-size: auto;">Progress</h2>
           <?php
 
@@ -108,7 +108,7 @@
       </div>
     </div>
 
-    
+
 
     <div class="col-md-3 col-12 bigScreenNav sticky" style="padding: 0; min-height:100vh">
 
@@ -126,14 +126,15 @@
   .galleryMobile {
     display: none;
   }
-@media only screen and (max-width: 768px) {
-.galleryDesktop {
-display: none;
-}
 
-.galleryMobile {
-display: initial;
-}
-}
+  @media only screen and (max-width: 768px) {
+    .galleryDesktop {
+      display: none;
+    }
+
+    .galleryMobile {
+      display: initial;
+    }
+  }
 </style>
 <?php get_footer(); ?>
