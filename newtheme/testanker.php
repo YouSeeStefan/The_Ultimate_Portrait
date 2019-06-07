1fe2f0937d70246fbc5426726b63c972047a46dd
Frank van der Velde <frankvdv1998@gmail.com>
2019-06-07 20:35:19 +0200

    File hernoemen naar juiste naam
    De file was bestemd voor het testen van de anker links in wordpress maar had een verkeerde naam.

---
+++ newtheme/testanker.php
@@ -0,0 +1,17 @@
+<?php /* Template Name: test */ ?>
+
+<html>
+  <body>
+  <!-- https://kinsta.com/blog/anchor-links/ -->
+
+
+  <ul>
+<li><a href="#manually-create-anchor-links-wordpress">How to Manually Create Anchor Links in WordPress</a></li>
+<li><a href="#anchor-links-wordpress-plugin">How to Create Anchor Links in WordPress with a Plugin</a></li>
+<li><a href="#anchor-links-wordpress-gutenberg">How to Create Anchor Links in WordPress with Gutenberg</a></li>
+</ul>
+  </body>
+</html>
+ 
+
+</body>
