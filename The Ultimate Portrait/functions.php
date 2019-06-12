c6302fe107770a78c6766c59b41f15bf9cd64b6a
FrankVanDerVelde <vrankvdv1998@gmail.com>
2019-06-12 12:34:22 +0200

    Clean up code.
    Removed unnused files, only load required files per page and clean up
    database.

--- The Ultimate Portrait/functions.php
+++ The Ultimate Portrait/functions.php
@@ -21,25 +21,11 @@ function load_stylesheets(){
   wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
   wp_enqueue_style('bootstrap');
 
-  wp_register_style('flexslider', get_template_directory_uri() . '/css/flexslider.css', array(), false, 'all');
-  wp_enqueue_style('flexslider');
-
-  wp_register_style('slick', get_template_directory_uri() . '/css/slick.css', array(), false, 'all');
-  wp_enqueue_style('slick');
-
-  wp_register_style('slicktheme', get_template_directory_uri() . '/css/slick-theme.css', array(), false, 'all');
-  wp_enqueue_style('slicktheme');
-
   wp_register_style('style', get_template_directory_uri() . '/style.css', array(), false, 'all');
   wp_enqueue_style('style');
 
-
-
   wp_register_style('navigation_style', get_template_directory_uri() . '/navigation.css', array(), false, 'all');
   wp_enqueue_style('navigation_style');
-
-  // wp_register_style('contact_popup', get_template_directory_uri() . '/contact_popup.css', array(), false, 'all');
-  // wp_enqueue_style('contact_popup');
 
   wp_register_style('home_style', get_template_directory_uri() . '/home.css', array(), false, 'all');
   wp_enqueue_style('home_style');
@@ -64,14 +50,22 @@ add_action('wp_enqueue_scripts', 'include_jquery');
 function loadjs(){
   wp_register_script('customjs', get_template_directory_uri() . '/js/script.js', '', 1, true);
   wp_enqueue_script('customjs');
-
-  wp_register_script('flexslider', get_template_directory_uri() . '/js/jquery.flexslider-min.js', '', 1, true);
-  wp_enqueue_script('flexslider');
-
-  wp_register_script('slick', get_template_directory_uri() . '/js/slick.min.js', '', 1, true);
-  wp_enqueue_script('slick');
 }
 add_action('wp_enqueue_scripts', 'loadjs');
+
+function load_slider(){
+ if (is_page_template('front-page.php')) {
+    wp_register_style('slick', get_template_directory_uri() . '/css/slick.css', array(), false, 'all');
+    wp_enqueue_style('slick');
+  
+    wp_register_style('slicktheme', get_template_directory_uri() . '/css/slick-theme.css', array(), false, 'all');
+    wp_enqueue_style('slicktheme');
+
+    wp_register_script('slick', get_template_directory_uri() . '/js/slick.min.js', '', 1, true);
+    wp_enqueue_script('slick');
+  }
+}
+add_action('wp_enqueue_scripts', 'load_slider');
 
 add_theme_support('menus');
 
