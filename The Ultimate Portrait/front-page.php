ca66678f211014dfbec28174de85025ff36d3624
Frank van der Velde <frankvdv1998@gmail.com>
2019-06-12 22:51:49 +0200

    Revert "Merge branch 'Frank' of https://github.com/YouSeeStefan/The_Ultimate_Portrait into Frank"
    This reverts commit b01a5beeca3533f26c56c3402046034e46c1b905, reversing
    changes made to 0d0b15b54dde91c1f30bf6fd5359720b9c07eaa8.

--- The Ultimate Portrait/front-page.php
+++ The Ultimate Portrait/front-page.php
@@ -53,41 +53,5 @@ Template Name: homepage
     </div>
   </div>
 </div>
-<script>
-$(document).ready(function(){
-  $(function () {
 
-      $("#contact-button").click(function() {
-      $(this).toggleClass("on");
-      $("#contact-popup").toggleClass("active");
-      });
-
-    });
-
-      $('.testimonials-container').slick({
-        slidesToShow: 1,
-        arrows: false,
-        autoplay: true,
-        dots: false,
-        autoplaySpeed: 3000,
-        responsive: [
-        {
-          breakpoint: 1250,
-          settings: {
-            slidesToShow: 1,
-            arrows: false,
-
-          }
-        },
-        {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 1,
-            arrows: false,
-          }
-        }
-        ]
-      });
-    });
-  </script>
 <?php get_footer(); ?>
