88343d77f2ca6d74c60eb5dc66fcbd89802dc0c9
Frank van der Velde <frankvdv1998@gmail.com>
2019-06-13 02:09:11 +0200

    Script bug test

--- The Ultimate Portrait/js/script.js
+++ The Ultimate Portrait/js/script.js
@@ -1,45 +1,4 @@
-//
-// $(window).on('load', function() {
-//   $('.flexslider').flexslider({
-//     animation: "slide"
-//   });
-// });
 
-$(document).ready(function(){
-  $(function () {
-
-      $("#contact-button").click(function() {
-      $(this).toggleClass("on");
-      $("#contact-popup").toggleClass("active");
-      });
-
-    });
-
-      $('.testimonials-container').slick({
-        slidesToShow: 1,
-        arrows: false,
-        autoplay: true,
-        dots: false,
-        autoplaySpeed: 3000,
-        responsive: [
-        {
-          breakpoint: 1250,
-          settings: {
-            slidesToShow: 1,
-            arrows: false,
-
-          }
-        },
-        {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 1,
-            arrows: false,
-          }
-        }
-        ]
-      });
-    });
 
     function openNav() {
       if (matchMedia) {
